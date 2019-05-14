3d7d88f83e18d395810264a41b7b8a5fbda4b30e
Ednil Libanio da Costa Junior <ednil.libanio@gmail.com>
2019-05-13 22:41:08 -0400

    incluido o campo T value que ajuda a definir um valor para usar na validação.

--- alrr-core-web-utils/src/main/java/br/leg/rr/al/core/web/util/CampoMetadata.java
+++ alrr-core-web-utils/src/main/java/br/leg/rr/al/core/web/util/CampoMetadata.java
@@ -5,32 +5,52 @@ package br.leg.rr.al.core.web.util;
  * @author Ednil
  *
  */
-public class CampoMetadata {
+public class CampoMetadata<T> {
+
+	private T value;
 
 	private Boolean required = false;
 	private Boolean disabled = false;
+
 	/**
 	 * @return the required
 	 */
 	public Boolean getRequired() {
 		return required;
 	}
+
 	/**
 	 * @param required the required to set
 	 */
 	public void setRequired(Boolean required) {
 		this.required = required;
 	}
+
 	/**
 	 * @return the disabled
 	 */
 	public Boolean getDisabled() {
 		return disabled;
 	}
+
 	/**
 	 * @param disabled the disabled to set
 	 */
 	public void setDisabled(Boolean disabled) {
 		this.disabled = disabled;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public T getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(T value) {
+		this.value = value;
 	}
 }
